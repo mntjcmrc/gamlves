1f4f2b3ff59da94e0dce965acb86f934c6992f87
mntjcmrc <mntjcmrc@gmail.com>
2013-05-13 01:00:22 +0200

    Sale el frame de login, es probable que no se añada más

--- src/org/gamlves/start/StartPoint.java
+++ src/org/gamlves/start/StartPoint.java
@@ -1,12 +1,16 @@
 package org.gamlves.start;
 
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+
+import org.gamlves.gui.LoginFrame;
 
 /**
  * Desde aquí se inicia el programa
  * 
  * @author mntjcmrc
- *
+ * 
  */
 public class StartPoint {
 
@@ -14,7 +18,12 @@ public class StartPoint {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				LoginFrame loginFrame = new LoginFrame();
+				loginFrame.setVisible(true);
+				loginFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
+		});
 	}
-
 }
