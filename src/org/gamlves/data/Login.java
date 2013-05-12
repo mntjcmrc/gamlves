b52d7c4e0d94542530f41658c4bc7a7ac2c20f5b
mntjcmrc <mntjcmrc@gmail.com>
2013-05-13 00:55:44 +0200

    Javadoc añadido

--- src/org/gamlves/data/Login.java
+++ src/org/gamlves/data/Login.java
@@ -1,6 +1,15 @@
 package org.gamlves.data;
 
+/**
+ * Se almacenará que usuario está actualmente logueado en el sistema
+ * 
+ * @author mntjcmrc
+ *
+ */
 public class Login {
 	
+	/**
+	 * Usuario actualmente logueado
+	 */
 	public static String _user;
 }
