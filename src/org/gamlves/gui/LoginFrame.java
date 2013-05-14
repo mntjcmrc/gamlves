1c49bd6756e066a497944914142fc09cf9aead3f
mntjcmrc <mntjcmrc@gmail.com>
2013-05-14 18:54:03 +0200

    Algo separada la ejecución del frame

--- src/org/gamlves/gui/LoginFrame.java
+++ src/org/gamlves/gui/LoginFrame.java
@@ -14,20 +14,34 @@ public class LoginFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = -9198884323424763366L;
-	protected JTextField txtUser, txtPass;
+	/**
+	 * Campos de texto con el usuario de login
+	 */
+	protected static JTextField txtUser;
+	/**
+	 * Campo de texto con la contraseña de login
+	 */
+	protected static JTextField txtPass;
+	/**
+	 * Etiquetas del usuario y contraseña
+	 */
 	private JLabel lblUser, lblPass;
+	/**
+	 * Botones de aceptar y cancelar
+	 */
 	private JButton btnAceptar, btnCancelar;
-//	private String usuario, pass;
+
+	// private String usuario, pass;
 
 	/**
-	 *  
+	 * Crea el frame de login con las etiquetas, campos de texto y botones
 	 */
 	public LoginFrame() {
-		
+
 		// Título y layout
 		setTitle("Login en Gamlves");
 		setLayout(new FlowLayout());
-		
+
 		// Tamaño
 		int widthFrame = 500;
 		int heightFrame = 120;
@@ -44,18 +58,16 @@ public class LoginFrame extends JFrame {
 		txtPass = new JPasswordField(15);
 		this.add(lblPass);
 		this.add(txtPass);
-		
+
 		// Botones
 		btnAceptar = new JButton("Aceptar");
 		btnCancelar = new JButton("Cancelar");
-		
+
 		btnAceptar.addActionListener(Actions.loginAceptar);
 		btnCancelar.addActionListener(Actions.cerrar);
-		
+
 		this.add(btnAceptar);
 		this.add(btnCancelar);
 
-		
-		
 	}
 }
--- src/org/gamlves/start/LoginRun.java
+++ src/org/gamlves/start/LoginRun.java
@@ -5,11 +5,28 @@ import javax.swing.JFrame;
 import org.gamlves.data.Datos;
 import org.gamlves.gui.LoginFrame;
 
+/**
+ * Desde aquí se ejecuta el frame de login, antes se cargan los datos de la base
+ * de datos en memoria
+ * 
+ * @author mntjcmrc
+ * 
+ */
 public class LoginRun implements Runnable {
+	/**
+	 * Frame de login
+	 */
 	public static LoginFrame loginFrame;
-
+	
 	public void run() {
 		Datos.loadData();
+		show();
+	}
+
+	/**
+	 * Crea y muestra el frame de login 
+	 */
+	private void show() {
 		loginFrame = new LoginFrame();
 		loginFrame.setVisible(true);
 		loginFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
